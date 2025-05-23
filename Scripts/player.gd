5db71a174165c90d6f09125aa2d8516c6fddddd3
Annis_William <anniw544@hsd.k12.or.us>
2025-05-23 11:41:55 -0700

    stamina system added w't aj help

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,8 +1,13 @@
 extends CharacterBody2D
+@onready var timer: Timer = $Timer
 
 @export var walk_speed = 0
 @export var sprint_mod = 0
 @export var deadzone = 0.5
+@export var max_stamina = 0
+@export var stamina_regen_rate = 0
+@export var Sstamina_drain_rate = 0
+var stamina = 0
 var rs_look = Vector2(0,0)
 
 #Player look
@@ -20,12 +25,30 @@ func _physics_process(delta):
 	if !Input.is_action_pressed("sprint"):
 		velocity.x = walk_speed * horizontal_direction
 		velocity.y = walk_speed * vertical_direction
-	elif Input.is_action_pressed("sprint"):
+	elif Input.is_action_pressed("sprint") && stamina > (Sstamina_drain_rate * 2):
 		velocity.x = (walk_speed * horizontal_direction) * sprint_mod
 		velocity.y = (walk_speed * vertical_direction) * sprint_mod
+		
+		
+	#Stamina manager
+	if !Input.is_action_pressed("sprint"):
+		if stamina < max_stamina:
+			stamina += stamina_regen_rate
+	if stamina < 0:
+		stamina = 0
+	elif stamina > max_stamina:
+		stamina = max_stamina
 	
+	if Input.is_action_just_pressed("sprint"):
+		timer.start()
+		
+	
+		
 	move_and_slide()
 	rslook()
+	print(stamina)
 
-	
+
+func _on_timer_timeout() -> void:
+		stamina -= Sstamina_drain_rate
 	
